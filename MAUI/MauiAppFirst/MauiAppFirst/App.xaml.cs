717b8baf86275f0bad1033837ba6fbcb7b029e69
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-11-11 14:09:49 +0700

    init

---
+++ MAUI/MauiAppFirst/MauiAppFirst/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppFirst
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
